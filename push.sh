6533bc1700e3055819ddf1c56dde48120e3458d6
stringmanolo <manuelvarelacaldas@gmail.com>
2024-06-27 19:57:05 +0200

    Add rm to .bash_history since is imposible to untrack it

--- push.sh
+++ push.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ -d small+FileSystem  ]]; then
+  rm small+FileSystem/root/.bash_history
   git add . && git commit -m "$1" && git push
 else
   echo "Missing small+FileSystem folder!";
