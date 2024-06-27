6123d2012304442a638fde957e9f8760a81f5b12
stringmanolo <manuelvarelacaldas@gmail.com>
2024-06-27 04:12:28 +0200

    Add a backup to revert changes if needed

--- exportInstalledSmall+.sh
+++ exportInstalledSmall+.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-7z a small+installed.7z /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/small+
+7z a -mx=9 small+installed.7z /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/small+
