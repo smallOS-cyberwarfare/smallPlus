c90bcb024ae8bce173acfda72519d6282f5d008c
stringmanolo <manuelvarelacaldas@gmail.com>
2024-06-28 11:27:44 +0200

    Update push

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ -d small+FileSystem  ]]; then
-  rm small+FileSystem/root/.bash_history
+  rm small+FileSystem/root/.bash_history 2>/dev/null 
   rm small+FileSystem/home/.npm -r
   rm small+FileSystem/home/.node_repl_history
   git add . && git commit -m "$1" && git push
