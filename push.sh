2ed29d71c3796db496cd70cb0d6d7e6e4209582c
stringmanolo <manuelvarelacaldas@gmail.com>
2024-06-28 11:25:16 +0200

    Update push

--- push.sh
+++ push.sh
@@ -2,6 +2,8 @@
 
 if [[ -d small+FileSystem  ]]; then
   rm small+FileSystem/root/.bash_history
+  rm small+FileSystem/home/.npm -r
+  rm small+FileSystem/home/.node_repl_history
   git add . && git commit -m "$1" && git push
 else
   echo "Missing small+FileSystem folder!";
