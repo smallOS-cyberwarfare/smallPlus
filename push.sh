bb6e0cc60fddd3301fc20f934537f61a8d9b9b6c
stringmanolo <manuelvarelacaldas@gmail.com>
2024-06-28 11:30:59 +0200

    Test

--- push.sh
+++ push.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
 if [[ -d small+FileSystem  ]]; then
-#  rm small+FileSystem/root/.bash_history 2>/dev/null 
-#  rm small+FileSystem/home/.npm -r 2>/dev/null
-#  rm small+FileSystem/home/.node_repl_history 2>/dev/null
+  rm small+FileSystem/root/.bash_history 2>/dev/null 
+  rm small+FileSystem/home/.npm -r 2>/dev/null
+  rm small+FileSystem/home/.node_repl_history 2>/dev/null
   git add . && git commit -m "$1" && git push
 else
   echo "Missing small+FileSystem folder!";
+fi
+
+
+if [[ -d small+FileSystem  ]]; then
+  echo '123'
 fi
