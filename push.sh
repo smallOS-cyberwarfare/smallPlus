be65811eb438c2d21b3a05fc5a7cdbae084214b2
stringmanolo <manuelvarelacaldas@gmail.com>
2024-06-28 11:48:30 +0200

    rm .viminfo

--- push.sh
+++ push.sh
@@ -4,6 +4,7 @@ if [[ -d small+FileSystem  ]]; then
   rm small+FileSystem/root/.bash_history 2>/dev/null 
   rm small+FileSystem/home/.npm -r 2>/dev/null
   rm small+FileSystem/home/.node_repl_history 2>/dev/null
+  rm small+FileSystem/home/.viminfo 2>/dev/null
   git add . &>/dev/null && git commit -m "$1" && git push
 else
   echo "Missing small+FileSystem folder!";
